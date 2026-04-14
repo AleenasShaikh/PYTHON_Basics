1300fcdaf38704fe6ea9fbdfcdaed026772590f0
Aleena <aleenashaikh795@gmail.com>
2026-04-14 20:58:53 +0530

    i wrote up code to print natural nmbers and even numbers.

--- 1.py
+++ 1.py
@@ -9,6 +9,12 @@ def subtract(a, b):
 def multiply(a, b):
     return print(a * b)
 
+def remainder(a, b):
+    return print(a % b)
+
+def concatinate (a, b):
+    return print(int(str(a) + str(b)))
+
 
 n1 = int(input("Enter first number: "))
 n2 = int(input("Enter second number: "))
@@ -16,10 +22,11 @@ n2 = int(input("Enter second number: "))
 operation = {
     "+": add,
     "-": subtract,
-    "*": multiply
+    "*": multiply,
+    "%": remainder
 }
 
-op = input("Enter the operation (+, -, *): ")
+op = input("Enter the operation (+, -, *, %): ")
 if op in operation:
     operation[op](n1, n2)
 else:    print("Invalid operation")
---
+++ 3.py
@@ -0,0 +1,34 @@
+
+
+
+def print_numbers():
+    a = 1
+    while True:
+        print(a)
+        a += 1
+        if a > 10:
+            break
+
+
+def print_number1():
+  
+    b = 1
+    while b < 167:
+        print(b)
+        b += 1
+
+def printevenno():
+    c = 2
+    while c < 167:
+    
+        c = c + 2
+        
+        print(c)
+
+
+
+# print_number1()
+# print_numbers()
+printevenno()
+
+
